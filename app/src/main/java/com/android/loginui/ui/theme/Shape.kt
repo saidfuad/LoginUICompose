5c6e1412ecea4a9d46fa649d19560ba631895e62
Said Fuad <saidfuad91@gmail.com>
2022-11-22 16:46:41 +0300

    Ver1.1 Commit to theme changes

--- app/src/main/java/com/android/loginui/ui/theme/Shape.kt
+++ app/src/main/java/com/android/loginui/ui/theme/Shape.kt
@@ -5,7 +5,8 @@ import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(24.dp),
+    small = RoundedCornerShape(23.dp),
     medium = RoundedCornerShape(4.dp),
     large = RoundedCornerShape(0.dp)
+    //smol = RoundedCornerShape(24.dp)
 )
